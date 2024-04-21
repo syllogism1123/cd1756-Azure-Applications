c55c18942cd8fba75d80e29bece979b8436181aa
syllogism1123 <kleine4augen@gmail.com>
2024-04-21 18:16:33 +0200

    commit

--- WRITEUP.md
+++ WRITEUP.md
@@ -2,11 +2,26 @@
 
 ### Analyze, choose, and justify the appropriate resource option for deploying the app.
 
-*For **both** a VM or App Service solution for the CMS app:*
-- *Analyze costs, scalability, availability, and workflow*
-- *Choose the appropriate solution (VM or App Service) for deploying the app*
-- *Justify your choice*
+## Virtual Machine (VM):
+
+- Cost: VMs generally require more management and maintenance, potentially increasing operational costs. You also have to consider the cost of the VM itself.
+- Scalability: VMs can be vertically or horizontally scaled but require manual intervention. Horizontal scaling might involve load balancers and more VM instances.
+- Availability: You need to manage backups, high availability configurations, and potentially implement failover strategies.
+- Workflow: Deployment can be more complex, involving provisioning, configuration, and ongoing maintenance tasks.
+
+## App Service:
+
+- Cost: App Service abstracts away infrastructure management, potentially reducing operational costs. You pay for the compute resources you use.
+- Scalability: App Service provides auto-scaling out-of-the-box based on demand.
+- Availability: Microsoft manages the underlying platform and ensures high availability.
+- Workflow: Deployment is simpler, with built-in CI/CD integration, making it easier to deploy updates.
+- 
+### Choose the appropriate solution (VM or App Service) for deploying the app:
+For a CMS app where rapid deployment, scalability, and cost-effectiveness are crucial, App Service would be the preferred option due to its managed nature, built-in scalability, and easier deployment workflows.
+
+### Justify your choice:
+App Service provides a managed environment that simplifies infrastructure management, reduces operational overhead, and allows for quicker deployments, making it an ideal choice for CMS applications that require agility, scalability, and cost-efficiency.
 
 ### Assess app changes that would change your decision.
+the CMS app requires specific configurations, dependencies, or performance optimizations that are not supported by App Service, such as custom networking configurations, specialized software installations, or GPU-intensive tasks, then a VM might become a more suitable option.
 
-*Detail how the app and any other needs would have to change for you to change your decision in the last section.* 
--- config.py
+++ config.py
@@ -1,25 +1,29 @@
 import os
+from urllib.parse import quote_plus
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'myarticle1123'
+    BLOB_STORAGE_KEY = os.environ.get(
+        'BLOB_STORAGE_KEY') or '40Rih1d4cDKIkn48vOswT+mSwAPKOUc8bMDGsVnsXMREYvVlcCeikuCKnMq7DWNKfOHBVTbSGKoX+AStVr5Rxg=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'article-cms-syl1123.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article-cms-db-1123'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '2L@559Fxjr6MEBK'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    encoded_password = quote_plus(SQL_PASSWORD)
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + ':' + encoded_password + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = "SGH8Q~uWoWPgBgHZkLRsJAYUvQfRjRpxslxbAbNg"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -27,15 +31,15 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "6ad76f32-b765-4ba2-bb3d-da26db82f2b6"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
+    SCOPE = ["User.Read"]  # Only need to read user profile for this app
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session

8d156f4dce29a9dc14ea17d75e7f27f4c52aa449
syllogism1123 <kleine4augen@gmail.com>
2024-04-22 13:17:08 +0200

    add log

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -65,19 +65,19 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        app.logger.info('Login succeeded')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            app.logger.warn('Login attempt failed')
+            app.logger.error('Login attempt failed')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('home')
+            app.logger.info('Login succeeded')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
